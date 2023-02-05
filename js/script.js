2923d446de71ab5aaa4ff6f1b24a3c4d5c31b1ae
PrestonC <1123preston1123@gmail.com>
2023-02-04 20:29:41 -0600

    changed page layout and styling

--- js/script.js
+++ js/script.js
@@ -32,3 +32,22 @@ $("#calculateExperience").on("click", function() {
 
     $("#calculationResult").text("Experience Points Needed: " + expNeeded.toLocaleString("en"));
 })
+
+/**
+ * Iterates through the main pages and hide them all
+ */
+function hideAllPages() {
+    $(".mainPage").each(function() {
+        $(this).hide();
+    });
+}
+
+/**
+ * Attach the click event to navigation links to hide all
+ * pages then show the one that matches the link pressed
+ */
+$('aside.sidebar__sidebar > a').on("click", function() {
+    hideAllPages();
+    // show the page of the link clicked on
+    $(this.dataset.target).show();
+})
