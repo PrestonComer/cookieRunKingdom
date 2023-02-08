6c5bdeb09d2e8a39e536e5a9b764416db8604b4f
PrestonC <1123preston1123@gmail.com>
2023-02-07 23:57:08 -0600

    debugging: maybe more $.each inside fetch .then

--- js/expCalculations.js
+++ js/expCalculations.js
@@ -68,22 +68,23 @@ function calculateExperience() {
 var cookieHouse;
 fetch("data/cookieHouses.json", { mode: "no-cors" })
     .then(res => res.json())
-    .then(data => { cookieHouse = data; });
-
-    console.log("start");
-    console.log("cookieHouse", cookieHouse);
-    $.each(cookieHouse.data, function(key, item) {
-        console.log("key", key);
-        console.log("item", item);
-        $("#cookieHouseTable > tbody").append("\
-            <tr>\
-                <td>{" + key + "</td>\
-                <td>{" + item[0] + "</td>\
-                <td>{" + item[1] + "</td>\
-            </tr>"
-        )
+    .then(data => {
+        cookieHouse = data;
+        console.log("start");
+        console.log("cookieHouse", cookieHouse);
+        $.each(cookieHouse.data, function(key, item) {
+            console.log("key", key);
+            console.log("item", item);
+            $("#cookieHouseTable > tbody").append("\
+                <tr>\
+                    <td>{" + key + "</td>\
+                    <td>{" + item[0] + "</td>\
+                    <td>{" + item[1] + "</td>\
+                </tr>"
+            )
+        });
+        console.log("end");
     });
-    console.log("end");
 
 //!-----Candy Count-----*//
 /** 
